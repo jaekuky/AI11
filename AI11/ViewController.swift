61a6add4edf430427ba3500e136223b08db227fc
jaekuky <jakuky@hanmail.net>
2023-04-25 23:38:06 +0900

    프로젝트 최초 생성

---
+++ AI11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AI11
+//
+//  Created by Jae kuk Ryu on 2023/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
